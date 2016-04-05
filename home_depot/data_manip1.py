83231f5aa464b08297d2ca2043c7ad40e8eacca5
JADALM <james.dipadua@gmail.com>
2016-04-05 21:44:59 +0100

    stopwords v1 working. will do stemming next.

--- home_depot/data_manip1.py
+++ home_depot/data_manip1.py
@@ -23,7 +23,7 @@ import enchant
 # for stemming and stopwords # PyStemmer + snowballstemmer 
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords  #  DON'T FORGET TO DOWNLOAD IT! :)  ## nltk.download("stopwords")
 
 # other utility libraries # might not use actually... 
 from operator import itemgetter
@@ -36,28 +36,44 @@ class Transformer:
 		self.set_intial_params()
 		self.read_file()
 		# self.initial_data_drop() # if applicable (place holder for future)
-		self.spell_check()
+		#self.spell_check()
 		self.gen_stems()
 		self.write_file()
 
 	def set_intial_params(self):
-		self.file_dir = "/data/"
+		self.file_dir = "data/"
 		self.raw_file_name = raw_input("Enter File Name (no directory):   ") 
 		self.version_num = "v" + raw_input("Version Number of Transformation: ")
 		self.fin_file = self.file_dir + self.raw_file_name + "_" + self.version_num
 
 	def read_file(self):
-		self.raw_data = pd.read_csv(self.root_dir+self.raw_file_name)
+		self.dataframe = pd.read_csv(self.file_dir+self.raw_file_name)
 
 	def spell_check(self):
+		# going to work on this part after steming ("just cuz")
 		self.spll_chk = enchant.Dict("en_US")
 
 	def gen_stems(self):
-		self.stopwrds = stopwords.words('english')
+		# before steming we'll remove stop words
+		self.stopwords_main = stopwords.words('english')
+		# mod stopwords a bit  # maybe later remove more measurement related words
+		#self.stopwords_alt = list('oz lbs. lbs ft ft. in. ml inch cu. cu ft. ft up cm oz. mm ounce no. of or gal. to'.split())
+		# not as surgical as it could be but "easier" for POC
+		if not self.dataframe['product_title'].empty:
+			self.dataframe['product_title'] = self.dataframe['product_title'].str.lower().str.split()
+			self.dataframe['product_title'].apply(lambda x: [item for item in x if item not in self.stopwords_main])
+		if not self.dataframe['search_term'].empty:
+			self.dataframe['search_term'] = self.dataframe['search_term'].str.lower().str.split()
+			self.dataframe['search_term'].apply(lambda x: [item for item in x if item not in self.stopwords_main])
+		# other stuff worth investigating is splitting on - becauset here are things like 3-piece BUT those could be useful so...yeah... 
+		# sanity check
+		#print self.dataframe['product_title']
+
+		# then we go on to stemming
 		self.stemmer = SnowballStemmer('english').stem
 
 	def write_file(self):
-
+		return 
 
 if __name__ == "__main__":
 	transformer = Transformer() 
