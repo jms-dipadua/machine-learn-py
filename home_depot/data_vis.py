07fa7559be125866a0c39ba6a2e0f4fc28061ea3
JADALM <james.dipadua@gmail.com>
2016-04-10 14:42:48 +0100

    update to notes plus some basic completeness stats on attribute data

--- home_depot/data_vis.py
+++ home_depot/data_vis.py
@@ -11,13 +11,13 @@ import seaborn as sns
 
 class Visualization:
 	def __init__(self):
-		get_params()
-		read_files()
-		merge_fields()
-		calc_basic_info()
+		self.get_params()
+		self.read_files()
+		self.merge_fields()
+		self.calc_basic_info()
 
 	def get_params(self):
-		self.root_dir = "/data"
+		self.root_dir = "data/"
 		self.train_fn = "train.csv"
 		self.test_fn = "test.csv"
 		self.attributes_fn = "attributes.csv"
@@ -35,34 +35,37 @@ class Visualization:
 		# TRAIN
 		self.train_df_m1 = self.train_df.merge(self.prod_desc_df, how='inner', on='product_uid')
 		self.train_df_m2 = self.train_df.merge(self.attr_df, how='inner', on='product_uid')
+		# verify merges
+		#print self.train_df_m1.columns.values
+		#print self.train_df_m2.columns.values
 		# TEST 
 		self.test_df_m1 = self.test_df.merge(self.prod_desc_df, how='inner', on='product_uid')
 		self.test_df_m2 = self.test_df.merge(self.attr_df, how='inner', on='product_uid')
 
 	def calc_basic_info(self):
 		# train
-		self.train_df_m1_complete_count = self.train_df_m1['attributes'].count(axis=1)
-		self.train_df_m1_length = self.train_df_m1.shape[1]
-		print "Overall completeness of attributes:  %f"  % (self.train_df_m1_complete_count / self.train_df_m1_length)
+		print "TRAIN STUFF"
+		print "Overall completeness of attributes:  %f"  % ( float(self.train_df_m2['value'].count()) / float(self.train_df_m2.shape[0]) )
 
 		# split train by "level" of relevance
 		# r1 = relevance == 1; r2 == 2, etc
-		self.train_df_m1_r1 = self.train_df_m1[self.train_df_m1['relevance'] == 1]
-		self.train_df_m1_r2 = self.train_df_m1[self.train_df_m1['relevance'] == 2]
-		self.train_df_m1_r3 = self.train_df_m1[self.train_df_m1['relevance'] == 3]
+		self.train_df_m2_r1 = self.train_df_m2[self.train_df_m2['relevance'] == 1]
+
+		self.train_df_m2_r2 = self.train_df_m2[self.train_df_m2['relevance'] == 2]
+		self.train_df_m2_r3 = self.train_df_m2[self.train_df_m2['relevance'] == 3]
 		# OUTPUT completeness
-		print "Relevance == 1 completeness of attributes:  %f"  % (self.train_df_m1_r1['attributes'].count(axis=1) / self.train_df_m1_r1.shape[1])
-		print "Relevance == 2 completeness of attributes:  %f"  % (self.train_df_m1_r2['attributes'].count(axis=1) / self.train_df_m1_r2.shape[1])
-		print "Relevance == 3 completeness of attributes:  %f"  % (self.train_df_m1_r3['attributes'].count(axis=1) / self.train_df_m1_r3.shape[1])
-
+		print "Relevance == 1 completeness of attributes:  %f"  % ( float(self.train_df_m2_r1['value'].count()) / float(self.train_df_m2_r1.shape[0]) )
+		print "Relevance == 2 completeness of attributes:  %f"  % ( float(self.train_df_m2_r2['value'].count()) / float(self.train_df_m2_r2.shape[0]) )
+		print "Relevance == 3 completeness of attributes:  %f"  % ( float(self.train_df_m2_r3['value'].count()) / float(self.train_df_m2_r3.shape[0]) )
 		#self.train_complete_plot = plt.boxplot(data)
-
 		# test 
-		self.test_df_m1_complete_count = self.test_df_m1['attributes'].count(axis=1)
-		self.test_df_m1_length = self.test_df_m1.shape[1]
+		print "TRAIN STUFF"
+		print "Overall completeness of attributes:  %f"  % ( float(self.test_df_m2['value'].count()) / float(self.test_df_m2.shape[0]) )
+
 
 	def create_charts(self):
 		#plt.show()
+		return
 
 if __name__ == "__main__":
 	visualization = Visualization() 
